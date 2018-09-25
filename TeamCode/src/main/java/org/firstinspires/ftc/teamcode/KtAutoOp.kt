68843dfbf3b033e0f74450877dffb365ac85ce97
KasaiYuki <shreyas.seethalla@gmail.com>
2018-09-25 18:56:16 -0400

    Created teleOp and autonomous based on current ideas autoop with unclamp, drive backwards and park in square

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KtAutoOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KtAutoOp.kt
@@ -5,7 +5,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.DcMotorSimple
+import com.qualcomm.robotcore.hardware.Servo
 import com.qualcomm.robotcore.util.ElapsedTime
+import com.sun.beans.editors.DoubleEditor
 
 /**
  * Created by KasaiYuki on 9/21/2018.
@@ -17,6 +21,15 @@ class KtAutoOp : LinearOpMode()
 {
     //adds robot objects for variables and methods
     val robot = Robot()
+
+    var extArm: DcMotor? = null
+    var armServo: Servo? = null
+
+    var motF = DcMotorSimple.Direction.FORWARD
+    var motR = DcMotorSimple.Direction.REVERSE
+    var serR = Servo.Direction.REVERSE
+    var serF = Servo.Direction.FORWARD
+
     @Throws(InterruptedException::class)
     override fun runOpMode()
     {
@@ -24,11 +37,29 @@ class KtAutoOp : LinearOpMode()
         telemetry.update()
         //initializes all parts
         robot.init(hardwareMap)
+        extArm = hardwareMap.dcMotor.get("extArm")
+        armServo = hardwareMap.servo.get("armServo")
+
+        extArm?.direction = motF
+        armServo?.direction = serF
 
         waitForStart()
         //runtime.reset()//IDK WHY RUNTIME DOESN'T WORK!
 
-        robot.driveForward(5.9)
-        robot.clampServo(.2)
+        unClamp()
+        robot.driveForward(-5.9)
+
+    }
+
+    fun unClamp()
+    {
+        extArm?.power = -1.0
+        armServo?.position = 0.0
+    }
+
+    fun drive(leftM: Double, rightM: Double)
+    {
+        robot.rightDrive.power = rightM
+        robot.leftDrive.power = leftM
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KtTeleOp.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/KtTeleOp.kt
@@ -19,12 +19,24 @@ class KtTeleOp : OpMode()
     //using robot class for motors, servos etc
     val robot = Robot()
 
+    var extArm: DcMotor? = null
+    var armServo: Servo? = null
+
+    var motF = DcMotorSimple.Direction.FORWARD
+    var motR = DcMotorSimple.Direction.REVERSE
+    var serR = Servo.Direction.REVERSE
+    var serF = Servo.Direction.FORWARD
     override fun init()
     {
         telemetry.addData("Status", "Initialized")
         telemetry.update()
         //initializes all parts
         robot.init(hardwareMap)
+        extArm = hardwareMap.dcMotor.get("extArm")
+        armServo = hardwareMap.servo.get("armServo")
+
+        extArm?.direction = motF
+        armServo?.direction = serF
     }
 
     override fun loop()
@@ -32,15 +44,19 @@ class KtTeleOp : OpMode()
         //Tank Drive
         var leftPower: Float = -gamepad1.left_stick_y
         var rightPower: Float = -gamepad1.right_stick_y
+        var armPower: Float = -gamepad2.left_stick_y
+        var extPower: Float = -gamepad2.right_stick_y
 
         robot.leftDrive.power = leftPower.toDouble()
         robot.rightDrive.power = rightPower.toDouble()
-
-        robot.arm.power = -gamepad2.left_stick_y.toDouble()
+        robot.arm.power = armPower.toDouble()
+        extArm?.power = extPower.toDouble()
+        spinServo()
 
         telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower)
         telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower)
         telemetry.addData("Arm Motor", " (%.2f)", -gamepad2.left_stick_y)
+        telemetry.addData("Servo", armServo?.position)
     }
 
     override fun stop()
@@ -48,4 +64,12 @@ class KtTeleOp : OpMode()
 
     }
 
+    private fun spinServo() {
+        if (gamepad2.left_bumper) {
+            armServo?.position = 0.25 //Sphere
+        } else if (gamepad2.right_bumper) {
+            armServo?.position = 0.45 //Cube
+        } else {
+            armServo?.position = 0.0 }
+    }
 }
